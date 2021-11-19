021869b839593e84bf575c189432eea70cd6feca
rezasalehi <rezasalehidev@gmail.com>
2021-11-19 22:38:41 +0330

    set Bootstrap for Project and utils

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-    return <div>App</div>;
+    return (
+        <div className="container-fluid">
+            <div className="col">1</div>
+            <div className="col">2</div>
+            <div className="col">3</div>
+        </div>
+    );
 }
 
 export default App;
