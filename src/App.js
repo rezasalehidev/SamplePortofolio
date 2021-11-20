cb305bb5e1a8752635d40b5648b830661d217730
rezasalehi <rezasalehidev@gmail.com>
2021-11-20 15:26:15 +0330

    Completed Header

--- src/App.js
+++ src/App.js
@@ -3,9 +3,64 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 import Navbar from "./components/Navbar";
 import "./App.css";
+import Header from "./components/Header";
+import Particles from "react-tsparticles";
 
 function App() {
-    return <Navbar />;
+    return (
+        <>
+            <Particles
+                id="tsparticles"
+                options={{
+                    background: {
+                        color: {
+                            value: "#fff",
+                        },
+                        opacity: 0.1,
+                    },
+                    particles: {
+                        color: {
+                            value: "#FFAB4C",
+                        },
+                        links: {
+                            color: "#FFAB4C",
+                            distance: 20,
+                            enable: true,
+                            opacity: 0.2,
+                            width: 1,
+                        },
+                        move: {
+                            direction: "none",
+                            enable: true,
+                            outMode: "bounce",
+                            random: false,
+                            speed: 2,
+                            straight: false,
+                        },
+                        number: {
+                            density: {
+                                enable: true,
+                                value_area: 900,
+                            },
+                            value: 20,
+                        },
+                        opacity: {
+                            value: 0.5,
+                        },
+                        shape: {
+                            type: "circle",
+                        },
+                        size: {
+                            random: true,
+                            value: 5,
+                        },
+                    },
+                }}
+            />
+            <Navbar />
+            <Header />
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div className="wraper">
+            <div className="header-main">
+                <h1>Welcome To My Website</h1>
+                <button type="button" class="btn btn-success mt-5">
+                    More Details
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
